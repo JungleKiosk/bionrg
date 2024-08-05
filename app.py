6eea8266ae0e6496cec541600be8fd1fb3a14f16
Fra <fra24eso@gmail.com>
2024-08-05 17:58:43 +0200

    first endpoint HOME

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return '<h1>HELLO</h1>'
-
+    return render_template('home.html')
 
