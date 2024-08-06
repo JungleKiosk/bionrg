2bb26ea79a8225e43016095b424c3b1a8bcc9f1a
Fra <fra24eso@gmail.com>
2024-08-06 12:01:42 +0200

    app.py: routes diets

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('home/home.html')
 
+@app.route('/dieta/<dieta_name>')
+def dieta(dieta_name):
+    dieta_name = dieta_name.upper()  # Converte il nome della dieta in maiuscolo
+    return render_template('diets/diet.html', dieta=f'Dieta {dieta_name}')
 
 if __name__ == '__main__':
     app.run(debug=True)
